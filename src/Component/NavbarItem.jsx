7ac2690d3523a789282ee2f6fbf4d675c9002636
Abdallah Askar <lalaskar678@gmail.com>
2025-04-16 14:19:10 +0200

    first commit

--- src/Component/NavbarItem.jsx
+++ src/Component/NavbarItem.jsx
@@ -58,7 +58,7 @@ const NavbarItem = () => {
                                     <li><Link to="/" >Home</Link></li>
                                     <li><Link to="/shop" >Shop</Link></li>
                                     <li><Link to="/blog" >Blog</Link></li>
-                                    {/* <li><Link to="/about" >About</Link></li> */}
+                                    <li><Link to="/about" >About</Link></li>
                                     <li><Link to="/contact" >Contact</Link></li>
 
 
